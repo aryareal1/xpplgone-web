ec0967a5073ce0b333f66cda29f0572cb0d076fc
aryareal <aryadwiputra193@gmail.com>
2025-09-06 17:15:19 +0700

    feat: import global pages
    - Imported global-not-found
    - Imported class-only
    Imported from the previous project

---
+++ app/class-only.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { LogInIcon } from 'lucide-react';
+import { Funnel_Display, Lexend } from 'next/font/google';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const lexend = Lexend({ subsets: ['latin'] });
+const funnel = Funnel_Display({ subsets: ['latin'] });
+
+export default function ClassOnly() {
+  const pathname = usePathname();
+
+  return (
+    <div className="absolute top-1/2 left-1/2 flex -translate-1/2 flex-col items-center text-center">
+      <h1
+        className={`${lexend.className} bg-gradient-to-r from-fuchsia-500 to-rose-500 bg-clip-text text-5xl font-bold text-transparent`}
+      >
+        FORBIDDEN
+      </h1>
+      <h2 className={`${funnel.className} text-lg`}>
+        Halaman ini dikhususkan untuk murid kelas X PPLG 1
+      </h2>
+      <p className={`${funnel.className} mt-5 text-gray-600 dark:text-gray-400`}>
+        Silahkan login untuk melanjutkan!
+      </p>
+      <Button variant="special" className="mt-2" asChild>
+        <Link href={`/login?r=${pathname}`}>
+          <LogInIcon /> Login to Continue
+        </Link>
+      </Button>
+    </div>
+  );
+}
---
+++ app/global-not-found.tsx
@@ -0,0 +1,40 @@
+import './globals.css';
+import { Funnel_Display, Lexend } from 'next/font/google';
+import type { Metadata } from 'next';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Button } from '@/components/ui/button';
+import { HomeIcon } from 'lucide-react';
+import Link from 'next/link';
+
+const lexend = Lexend({ subsets: ['latin'] });
+const funnel = Funnel_Display({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Not Found | X PPLG 1',
+  description: "The page you're looking for doesn't exist.",
+};
+
+export default function GlobalNotFound() {
+  return (
+    <html lang="id" suppressHydrationWarning>
+      <head />
+      <body className="flex h-dvh flex-col items-center justify-center gap-2 antialiased">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <h1
+            className={`${lexend.className} bg-gradient-to-r from-fuchsia-500 to-rose-500 bg-clip-text text-7xl font-bold text-transparent`}
+          >
+            404
+          </h1>
+          <p className={`${funnel.className} text-lg`}>
+            The page you&apos;re looking for dosen&apos;t exist!
+          </p>
+          <Button variant="special" className="mt-2" asChild>
+            <Link href="/">
+              <HomeIcon /> Back to Homepage
+            </Link>
+          </Button>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
