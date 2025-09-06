ed4d84330864fd89655bbbe8e6036a4d17b6cdba
aryareal <aryadwiputra193@gmail.com>
2025-09-06 17:14:09 +0700

    build: update some dependencies
    - Imported all dependencies from the previous project
    - Changes eslint and prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,6 +23,7 @@ const eslintConfig = [
       'jsx-a11y/aria-unsupported-elements': 'warn',
       'jsx-a11y/role-has-required-aria-props': 'warn',
       'jsx-a11y/role-supports-aria-props': 'warn',
+      'prefer-const': 'off',
     },
   }),
   {
