c8d443d60c9fe54738650254ff7d8bda96f1c24f
aryareal <aryadwiputra193@gmail.com>
2025-12-19 21:24:55 +0700

    fix: revert: remove work in progress funds

--- app/(root)/funds/page.tsx
+++ app/(root)/funds/page.tsx
@@ -12,7 +12,7 @@ import {
 } from 'ag-grid-community';
 import { useUser } from '@/hooks/use-user';
 import SectionHeader from '@/components/section-header';
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { createClient } from '@/lib/supabase/client';
 import { IFundsData, IFundsDate } from '@/app/api/funds/route';
 import { useStudents } from '@/hooks/use-students';
@@ -38,9 +38,6 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { useTheme } from 'next-themes';
-import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
-import { StatisticsDialog } from './dialogs';
-import { distribute } from '@/lib/utils';
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
@@ -88,130 +85,87 @@ export default function FundsPage() {
   );
   const gridRef = useRef<AgGridReact>(null); // eslint-disable-next-line react-hooks/exhaustive-deps
   const gapi = useMemo(() => gridRef.current?.api, [gridRef.current]);
-  const dateColDef = useCallback(
-    (date: IFundsDate) => {
-      const isOld = new Date(date.date).getTime() < new Date(new Date().toDateString()).getTime();
-
-      return {
-        field: date.date,
-        cellClass: 'text-center',
-        cellClassRules: {
-          'text-yellow-500': (params) =>
-            Array.isArray(params.value) && params.value[0] !== 2000 && isOld,
-          'text-green-400': (params) =>
-            (params.value && isOld) || (Array.isArray(params.value) && params.value[0] === 2000),
-          'text-red-500': (params) => !params.value && isOld,
-        },
-        enableCellChangeFlash: true,
-        valueFormatter: (params) => {
-          const v = params.data[date.date];
-          if (!v) {
-            if (isOld) return '-';
-            return;
-          }
-          return Array.isArray(v)
-            ? v[0] === 2000
-              ? '✓'
-              : (v[0] * -1).toLocaleString('id-ID')
-            : v.toLocaleString('id-ID');
-        },
-        headerComponent: DateDisplay,
-        headerComponentParams: {
-          date: date.date,
-        },
-        width: 82,
-        sortable: false,
-        suppressMovable: true,
-        resizable: false,
-        ...(isEditor
-          ? {
-              editable: (params) => {
-                if (isOld) return false;
-                const v = params.data[date.date];
-                return !(Array.isArray(v) && v[0] === 2000);
-              },
-              cellEditor: 'agNumberCellEditor',
-              cellEditorParams: {
-                precision: 0,
-              } as INumberCellEditorParams,
-            }
-          : {}),
-      } as ColDef;
-    },
-    [isEditor]
-  );
 
   // Database
   const supabase = createClient();
   const [data, setData] = useState<IFundsData[]>([]);
-  const [dates, setDates] = useState<IFundsDate[]>([]);
   const [students] = useStudents();
-  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     if (!gapi) return;
-    if (!mounted) {
-      fetch('/api/funds')
-        .then((r) => r.json())
-        .then((d) => {
-          const { data, dates } = d;
-          setData(data);
-          setDates(dates);
-          setMounted(true);
-        });
-      return;
-    }
+    fetch('/api/funds')
+      .then((r) => r.json())
+      .then(({ data, dates }: { data: IFundsData[]; dates: IFundsDate[] }) => {
+        setData(data);
 
-    gapi.setGridOption(
-      'rowData',
-      students.map((s, i) => {
-        let row = {
-          no: i + 1,
-          name: s.full_name,
-          uid: s.uid,
-        };
-        let list = dates.map(
-          (dt) => data.find((d) => d.user === s.uid && d.date === dt.date)?.amount || null
+        gapi.setGridOption(
+          'rowData',
+          students.map((s, i) => {
+            let row = {
+              no: i + 1,
+              name: s.full_name,
+              uid: s.uid,
+            };
+            data
+              .filter((d) => d.user === s.uid)
+              // @ts-expect-error "`d.date` is valid for index"
+              .forEach((d) => (row[d.date] = d.amount));
+            return row;
+          })
         );
-        distribute(list, 2000).forEach((v, i) => {
-          const d = dates[i];
-          // @ts-expect-error "`d.date` is valid for index"
-          row[d.date] = v;
-        });
-        return row;
-      })
-    );
 
-    gapi.setGridOption('columnDefs', [
-      {
-        field: 'no',
-        width: 42,
-        sortable: false,
-        resizable: false,
-        suppressMovable: true,
-        pinned: 'left',
-        cellClass: 'text-center',
-      },
-      {
-        field: 'name',
-        headerName: 'Nama',
-        width: 220,
-        suppressMovable: true,
-        pinned: 'left',
-      },
-      ...dates.map<ColDef>((d) => dateColDef(d)),
-      ...(isEditor
-        ? [
-            {
-              field: 'add',
-              width: 82,
-              resizable: false,
-              suppressMovable: true,
-              headerComponent: AddDateButton,
+        gapi.setGridOption('columnDefs', [
+          {
+            field: 'no',
+            width: 42,
+            sortable: false,
+            resizable: false,
+            suppressMovable: true,
+            pinned: 'left',
+            cellClass: 'text-center',
+          },
+          {
+            field: 'name',
+            headerName: 'Nama',
+            width: 220,
+            suppressMovable: true,
+            pinned: 'left',
+          },
+          ...dates.map<ColDef>((d) => ({
+            field: d.date,
+            enableCellChangeFlash: true,
+            valueFormatter: (params) => params.data[d.date]?.toLocaleString('id-ID'),
+            headerComponent: DateDisplay,
+            headerComponentParams: {
+              date: d.date,
             },
-          ]
-        : []),
-    ]);
+            width: 82,
+            sortable: false,
+            suppressMovable: true,
+            resizable: false,
+            ...(isEditor
+              ? {
+                  editable: true,
+                  cellEditor: 'agNumberCellEditor',
+                  cellEditorParams: {
+                    precision: 0,
+                  } as INumberCellEditorParams,
+                }
+              : {}),
+          })),
+          ...(isEditor
+            ? [
+                {
+                  field: 'add',
+                  width: 82,
+                  resizable: false,
+                  suppressMovable: true,
+                  headerComponent: AddDateButton,
+                },
+              ]
+            : []),
+        ]);
+      });
 
     const ch = supabase
       .channel('funds', { config: { private: true } })
@@ -223,48 +177,45 @@ export default function FundsPage() {
 
           if (table === 'data') {
             const record = (payload.record || payload.old_record) as IFundsData;
-            setData((data) => {
-              if (operation === 'INSERT') return [...data, record];
-              else if (operation === 'UPDATE')
-                return data.with(
+            let row = gapi.getRowNode(record?.user);
+            row?.setDataValue(record.date, (payload.record as IFundsData | null)?.amount);
+
+            if (operation === 'INSERT') setData((data) => [...data, record]);
+            else if (operation === 'UPDATE')
+              setData((data) =>
+                data.with(
                   data.findIndex((w) => w.id === record.id),
                   record
-                );
-              else if (operation === 'DELETE') return data.filter((w) => w.id !== record.id);
-              return data;
-            });
-
-            let row = gapi.getRowNode(record?.user);
-            let rowData = { ...row?.data };
-            let list = dates.map((dt) =>
-              dt.date === record.date
-                ? record.amount
-                : data.find((d) => d.user === rowData.uid && d.date === dt.date)?.amount || null
-            );
-            distribute(list, 2000).forEach((v, i) => {
-              const d = dates[i];
-              rowData[d.date] = v;
-            });
-            row?.setData(rowData);
+                )
+              );
+            else if (operation === 'DELETE')
+              setData((data) => data.filter((w) => w.id !== record.id));
           }
 
           if (table === 'dates') {
             const record = (payload.record || payload.old_record) as IFundsDate;
-            setDates((dates) => {
-              let n = dates.slice();
-              if (operation === 'INSERT') n = [...dates, record];
-              else if (operation === 'UPDATE')
-                n = dates.with(
-                  dates.findIndex((w) => w.date === record.date),
-                  record
-                );
-              else if (operation === 'DELETE') n = dates.filter((w) => w.date !== record.date);
-              n.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
-              return n;
-            });
-
             let columns = gapi.getColumnDefs() || [];
-            let newColumns = dateColDef(record);
+            let newColumns: ColDef = {
+              field: record.date,
+              enableCellChangeFlash: true,
+              headerComponent: DateDisplay,
+              headerComponentParams: {
+                date: record.date,
+              },
+              width: 82,
+              sortable: false,
+              suppressMovable: true,
+              resizable: false,
+              ...(isEditor
+                ? {
+                    editable: true,
+                    cellEditor: 'agNumberCellEditor',
+                    cellEditorParams: {
+                      precision: 0,
+                    } as INumberCellEditorParams,
+                  }
+                : {}),
+            };
 
             if (operation === 'INSERT')
               columns = [...columns.slice(0, -1), newColumns, ...columns.slice(-1)];
@@ -290,14 +241,13 @@ export default function FundsPage() {
     return () => {
       supabase.removeChannel(ch);
     };
-  }, [data, dateColDef, dates, gapi, isEditor, mounted, students, supabase]);
+  }, [gapi, isEditor, students, supabase]);
 
   // State
   const [contextMenuPos, setContextMenuPos] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
   const [contextMenu, setContextMenu] = useState<
-    CellContextMenuEvent | ColumnHeaderContextMenuEvent
-  >();
-  const [contextMenuOpen, setContextMenuOpen] = useState(false);
+    CellContextMenuEvent | ColumnHeaderContextMenuEvent | null
+  >(null);
 
   return (
     <main className="mx-auto mt-5 flex max-w-[90rem] flex-col gap-5 px-4">
@@ -320,10 +270,10 @@ export default function FundsPage() {
             getRowId={(params) => params.data.uid}
             onCellValueChanged={(ev) => {
               if (!ev.source) return;
+              console.log(ev);
               let [uid, date] = [ev.data.uid, ev.column.getId()];
               let record = data.find((d) => d.user === uid && d.date === date);
               let value = ev.value;
-              if (!record && !value) return;
 
               // INSERT
               if (!record && value)
@@ -338,7 +288,6 @@ export default function FundsPage() {
                     },
                   }),
                 });
-
               // DELETE
               if (record && !value)
                 return fetch('/api/funds', {
@@ -367,13 +316,11 @@ export default function FundsPage() {
             onCellContextMenu={(ev) => {
               if (ev.column.getId() === 'add') return;
               setContextMenu(ev);
-              setContextMenuOpen(true);
             }}
             preventDefaultOnContextMenu
             onColumnHeaderContextMenu={(ev) => {
               if (ev.column.getId() === 'add') return;
               setContextMenu(ev);
-              setContextMenuOpen(true);
             }}
             onCellEditingStarted={(params) => {
               const editor = params.api.getCellEditorInstances({
@@ -384,100 +331,84 @@ export default function FundsPage() {
             }}
           />
 
-          <Dialog>
-            {contextMenuOpen && contextMenu && (
-              <DropdownMenu open onOpenChange={() => setContextMenuOpen(false)}>
-                <DropdownMenuTrigger asChild>
-                  <div
-                    style={{
-                      position: 'absolute',
-                      top: contextMenuPos.y,
-                      left: contextMenuPos.x,
-                    }}
-                  />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                  className="font-outfit font-light dark:bg-gray-800"
-                  side="right"
-                  align="start"
-                >
-                  {'data' in contextMenu ? (
-                    <>
-                      {!['no', 'name'].includes(contextMenu.column.getId()) && (
-                        <>
-                          {isEditor && (
-                            <DropdownMenuItem
-                              className="focus:bg-gray-125 cursor-pointer text-sm dark:focus:bg-gray-700"
-                              onClick={() =>
-                                contextMenu.api.startEditingCell({
-                                  colKey: contextMenu.column.getId(),
-                                  rowIndex: contextMenu.rowIndex || 0,
-                                  rowPinned: contextMenu.rowPinned,
-                                  key: 'Enter',
-                                })
-                              }
-                            >
-                              <PencilLineIcon /> Edit
-                            </DropdownMenuItem>
-                          )}
-                          <DropdownMenuItem className="focus:bg-gray-125 cursor-pointer text-sm dark:focus:bg-gray-700">
-                            <ReceiptTextIcon /> Detail
+          {contextMenu && (
+            <DropdownMenu open onOpenChange={() => setContextMenu(null)}>
+              <DropdownMenuTrigger asChild>
+                <div
+                  style={{
+                    position: 'absolute',
+                    top: contextMenuPos.y,
+                    left: contextMenuPos.x,
+                  }}
+                />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                className="font-outfit font-light dark:bg-gray-800"
+                side="right"
+                align="start"
+              >
+                {'data' in contextMenu ? (
+                  <>
+                    {!['no', 'name'].includes(contextMenu.column.getId()) && (
+                      <>
+                        {isEditor && (
+                          <DropdownMenuItem
+                            className="focus:bg-gray-125 cursor-pointer text-sm dark:focus:bg-gray-700"
+                            onClick={() =>
+                              contextMenu.api.startEditingCell({
+                                colKey: contextMenu.column.getId(),
+                                rowIndex: contextMenu.rowIndex || 0,
+                                rowPinned: contextMenu.rowPinned,
+                                key: 'Enter',
+                              })
+                            }
+                          >
+                            <PencilLineIcon /> Edit
                           </DropdownMenuItem>
-                          <DropdownMenuSeparator />
-                        </>
-                      )}
-                      <DialogTrigger asChild>
+                        )}
                         <DropdownMenuItem className="focus:bg-gray-125 cursor-pointer text-sm dark:focus:bg-gray-700">
-                          <ChartNoAxesColumnIcon /> Statistik
+                          <ReceiptTextIcon /> Detail
                         </DropdownMenuItem>
-                      </DialogTrigger>
-                      <DropdownMenuItem className="focus:bg-gray-125 cursor-pointer text-sm dark:focus:bg-gray-700">
-                        <User2Icon /> Profil
+                        <DropdownMenuSeparator />
+                      </>
+                    )}
+                    <DropdownMenuItem className="focus:bg-gray-125 cursor-pointer text-sm dark:focus:bg-gray-700">
+                      <ChartNoAxesColumnIcon /> Statistik
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="focus:bg-gray-125 cursor-pointer text-sm dark:focus:bg-gray-700">
+                      <User2Icon /> Profil
+                    </DropdownMenuItem>
+                  </>
+                ) : (
+                  <>
+                    {!['no', 'name'].includes(contextMenu.column.getId()) && isEditor && (
+                      <DropdownMenuItem
+                        className="focus:bg-gray-125 cursor-pointer text-sm dark:focus:bg-gray-700"
+                        onClick={() => {
+                          fetch('/api/funds', {
+                            method: 'POST',
+                            body: JSON.stringify({
+                              table: 'dates',
+                              delete: true,
+                              value: { date: contextMenu.column.getId() },
+                            }),
+                          });
+                        }}
+                      >
+                        <Trash2Icon /> Hapus
                       </DropdownMenuItem>
-                    </>
-                  ) : (
-                    <>
-                      {!['no', 'name'].includes(contextMenu.column.getId()) && isEditor && (
-                        <DropdownMenuItem
-                          className="focus:bg-gray-125 cursor-pointer text-sm dark:focus:bg-gray-700"
-                          onClick={() => {
-                            fetch('/api/funds', {
-                              method: 'POST',
-                              body: JSON.stringify({
-                                table: 'dates',
-                                delete: true,
-                                value: { date: contextMenu.column.getId() },
-                              }),
-                            });
-                          }}
-                        >
-                          <Trash2Icon /> Hapus
-                        </DropdownMenuItem>
-                      )}
-                    </>
-                  )}
-                </DropdownMenuContent>
-              </DropdownMenu>
-            )}
-            <DialogContent className="font-outfit">
-              {contextMenu && (
-                <>
-                  {'data' in contextMenu ? (
-                    <StatisticsDialog data={data} dates={dates} contextMenu={contextMenu} />
-                  ) : (
-                    <></>
-                  )}
-                </>
-              )}
-            </DialogContent>
-          </Dialog>
+                    )}
+                  </>
+                )}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
         </div>
       </section>
     </main>
   );
 }
 
-// Date Grid Header
 const days = ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'];
 function DateDisplay(props: { date: string }) {
@@ -492,7 +423,6 @@ function DateDisplay(props: { date: string }) {
   );
 }
 
-// Add Date Popup Button
 function AddDateButton() {
   const [date, setDate] = useState(new Date());
   const [month, setMonth] = useState(new Date());
