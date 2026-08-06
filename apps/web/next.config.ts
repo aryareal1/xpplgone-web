aec660e439e3672ae387d93a9eb099238774470e
AryaReal <aryadwiputra193@gmail.com>
2026-08-06 09:13:08 +0700

    chore(web): cleanup deps use default next app dependencies to make sure stable. cleanup some deps and shadcn components

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
   experimental: {
     globalNotFound: true,
-    useTypeScriptCli: true,
   },
   images: {
     remotePatterns: [
